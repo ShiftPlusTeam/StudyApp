11a628e7da834b8f9556c0cb26f26165705f70e4
Hirofumi <sasa1235711@gmail.com>
2017-08-07 23:33:31 +0900

    AppDataControllerクラス作成

---
+++ StudyApp/StudyApp/AppDataController.swift
@@ -0,0 +1,44 @@
+//
+//  AppDataController.swift
+//  StudyApp
+//
+//  Created by Sasakura Hirofumi on 2017/08/07.
+//  Copyright © 2017年 ShiftPlus+. All rights reserved.
+//
+
+import Foundation
+
+//各画面で必要となるデータを扱うクラス
+
+class  AppDataController {
+    
+    // Field
+    
+    // 資格名
+    // ex) CCNA LPIC
+    var name: String = "資格名"
+    
+    // ジャンル
+    // ex) Network Database
+    var genre: [String] = []
+    
+    
+    // 問題数
+    // ex) 5 10 All
+    var count: String = "5"
+    
+    // 問題の種別
+    // 全問 未回答 不正解
+    var kind: String = "全問"
+    
+    
+    
+    
+    
+    // Method
+    
+    
+    
+    
+    
+}
