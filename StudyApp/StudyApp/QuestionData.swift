a0dc4b04994d996b9a2a3d04574e050fd62ea634
Yuta Dosho <cloud9nueve@me.com>
2017-08-27 11:53:22 +0900

    途中保存

--- StudyApp/StudyApp/QuestionData.swift
+++ StudyApp/StudyApp/QuestionData.swift
@@ -12,11 +12,88 @@ import AudioToolbox
 //1つの問題に関する情報を格納するクラス
 class QuestionData {
     
+    init(questionSourceDataArray: [String]) {
+        question = questionSourceDataArray[0]
+        optionA = questionSourceDataArray[1]
+        optionB = questionSourceDataArray[2]
+        optionC = questionSourceDataArray[3]
+        optionD = questionSourceDataArray[4]
+        correctAnswer = questionSourceDataArray[5]
+        comment = questionSourceDataArray[6]
+    }
+    
+    //　シングルトンオブジェクトの宣言
+    static let sharedInstance = QuestionData()
+    
+    var questionDataArray = [QuestionData]()
+    var nowQuestionIndex: Int = 0
+    
+    // 初期化処理
+    // シングルトンを保証するためにprivateで宣言する
+    private init() {
+        
+    }
+    
+    // CSVファイルから問題文を読み込む処理（relumeはようわからん）
+    func loadQuestion() {
+        
+        // 格納済みの問題文の削除
+        questionDataArray.removeAll()
+        
+        // 問題文のIndexを初期化
+        nowQuestionIndex = 0
+        
+        // CSVファイルパスの取得
+        guard let csvFilePath = Bundle.main.path(forResource: "question", ofType: "csv") else {
+            print("csvファイルが存在しません")
+            return
+        }
+        // ファイル内容の取得
+        do {
+            let csvStringData = try String(contentsOfFile: csvFilePath, encoding: String.Encoding.utf8)
+            
+            // csvデータを1行ずつ読み込む
+            csvStringData.enumerateLines(invoking: { (line, stop) in
+                // カンマ区切りで分割
+                let questionSourceDataArray = line.components(separatedBy: ",")
+                // 問題データを格納するオブジェクトを作成
+                let questionData = QuestionData(questionSourceDataArray: questionSourceDataArray)
+                // 問題を追加
+                self.questionDataArray.append(questionData)
+                // 問題番号を設定
+                questionData.questionNo = self.questionDataArray.count
+            })
+        } catch let error {
+            print("csvファイル読み込みエラーが発生しました:\(error)")
+            return
+        }
+    }
+    
     //問題文
     var question: String = "問題文"
     
+    //問題文のgetter
+    public func getQuestionName() -> (String) {
+        return question
+    }
+    
+    //問題文のsetter
+    public func setQuestionName(question: String) {
+        let question = QuestionData.question
+    }
+    
     //解説文
     var comment: String = "解説文"
+    
+    //問題文のgetter
+    public func getCommentName() -> (String) {
+        return comment
+    }
+    
+    //問題文のsetter
+    public func setCommentName(question: String) {
+        let question = QuestionData.comment
+    }
     
     //選択肢A
     var optionA: String = "A"
@@ -31,26 +108,13 @@ class QuestionData {
     var optionD: String = "D"
     
     //正解の番号
-    var correctAnswer: String = "A"
+    var correctAnswer: String = "X"
     
     //選択された番号
-    var ChoiceAnswer: String = "A"
+    var ChoiceAnswer: String = "Y"
     
     //問題の番号
-    var questionNo: String = "A"
-    
-    
-    //クラスが生成されたときの処理
-    //    init(questionSourceDataArray: [String]) {
-    //        question = questionSourceDataArray[0]
-    //        answerA = questionSourceDataArray[1]
-    //        answerB = questionSourceDataArray[2]
-    //        answerC = questionSourceDataArray[3]
-    //        answerD = questionSourceDataArray[4]
-    //        correctAnswer = Int(questionSourceDataArray[5])!
-    //    }
-    //
-    
+    var questionNo: Int = 0
     
     //選択された番号が正解か判定する
     func isCorrect() -> Bool {
@@ -65,94 +129,19 @@ class QuestionData {
         //不正解
         return false
     }
-}
-
-
-//クイズデータの管理クラス
-class QuestionDataManager {
-    
-    //シングルトン
-    static let sharedinstance = QuestionDataManager()
-    
-    //問題文を格納する配列
-    var questionDataArray = [QuestionData]()
-    
-    //現在の問題のインデックス
-    var  nowQuestionIndex: Int = 0
-    
-    //初期化
-    private init() {
-        
-    }
-    //問題文の読み込み,getter
-    func loadQuestion () {
-        //格納済みの問題は削除
-        questionDataArray.removeAll()
-        //現在の問題のインデックスの初期化
-        nowQuestionIndex = 0
-        
-        //csvファイルパス
-        guard let csvFilePath = Bundle.main.path(forResource: "question",ofType: "csv") else {
-            //csvファイルなし
-            print("csvファイルが存在しません")
-            return
-        }
-        
-        //csvファイル読み込み
-        do {
-            let csvStringData = try String(contentsOfFile: csvFilePath, encording: String.Encoding.utf8)
-            //csvを1行ずつ読み込む
-            csvStringData.enumerateLines(invoking: { (line, stop) in let questionSourceDataArray = line.components(separatedBy: ",")
-                //問題文を格納するオブジェクト
-                let questionData = QuestionData(questtionSourceDataArry: questionSourceDataArray)
-                //問題を追加
-                self.questionDataArray.append(questionData)
-            })
-        } catch let error {
-            print("csvファイル読み込みエラー:\(error)")
-            return
-        }
-        
-    }
-    
-    //次の問題を取り出す
-    func nextQuestion() -> QuestionData? {
-        if nowQuestionIndex < questionDataArray.count {
-            let nextquestion = questionDataArray[nowQuestionIndex]
-            nowQuestionIndex += 1
-            return nextquestion
-        }
-        return nil
-    }
     
     
-}
-
-class QuestionViewController: UIViewController {
     
-    var questionData: QuestionData!
-    @IBOutlet weak var questionNoLabel: UILabel!      //問題番号ラベル
-    @IBOutlet weak var questionTextView: UITextView!  //問題文テキスト
-    @IBOutlet weak var OptionAButton: UIButton!       //選択肢A
-    @IBOutlet weak var OptionBButton: UIButton!       //選択肢B
-    @IBOutlet weak var OptionCButton: UIButton!       //選択肢C
-    @IBOutlet weak var OptionDButton: UIButton!       //選択肢D
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //初期データ設定、問題番号、問題文、選択肢のSetter
-        questionNoLabel.text = "Q.\(questionData.questionNo)"
-        questionTextView.text = questionData.question
-        OptionAButton.setTitle(questionData.optionA, for: UIControlState.normal)
-        OptionBButton.setTitle(questionData.optionB, for: UIControlState.normal)
-        OptionCButton.setTitle(questionData.optionC, for: UIControlState.normal)
-        OptionDButton.setTitle(questionData.optionD, for: UIControlState.normal)
-    }
+    
+    
     
     
     
 }
+
+
+
 
 
 
