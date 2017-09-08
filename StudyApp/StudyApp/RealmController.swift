c1c6ad06305090f23d201827d44bfdf24d23ea69
正之 <masayuki@masayukinoMacBook-Pro.local>
2017-09-09 08:00:35 +0900

    リルムクラス仕様変更：問題テーブルのカラム制定、ゲッターの返り値をList型に変更、メソッド追加 機能追加：ランダムソート機能、問題個数指定機能追加、呼び出される場合を想定して検索文入力を省略し、その分メソッドを作った

--- StudyApp/StudyApp/RealmController.swift
+++ StudyApp/StudyApp/RealmController.swift
@@ -22,43 +22,53 @@ class License: Object  {
     
 }
 
-class QesList :Object{
+class Question :Object{
     //問題一覧
     //No:問題番号
+    //LicenseId:資格ID
     //Genre:ジャンル
     //Problem:問題文
-    //Comment:?
-    //Option1:選択肢
-    //Option2:選択肢
-    //Option3:選択肢
-    //Option4:選択肢
-    //Answer:正解
-    //Correct:?
+    //Comment:解説文
+    //OptionA:選択肢
+    //OptionB:選択肢
+    //OptionC:選択肢
+    //OptionD:選択肢
+    //Answer:正解の選択肢
+    //Correct:過去に正解したかどうか
+    //Done:過去に回答したかどうか
 
-    //
     dynamic var No :Int8 = 0
+    dynamic var LicenseId :Int8 = 0
     dynamic var Genre :String = ""
     dynamic var Problem :String = ""
     dynamic var Comment :String = ""
-    dynamic var Option1 :String = ""
-    dynamic var Option2 :String = ""
-    dynamic var Option3 :String = ""
-    dynamic var Option4 :String = ""
+    dynamic var OptionA :String = ""
+    dynamic var OptionB :String = ""
+    dynamic var OptionC :String = ""
+    dynamic var OptionD :String = ""
     dynamic var Answer :String = ""
     dynamic var Correct :Bool = false
-    dynamic var Answered :Bool = false
+    dynamic var Done :Bool = false
     
 }
 
-class RealmControllerLisence{
+class RealmControllerLicense{
     
     let realm = try! Realm()
     let result = try! Realm().objects(License.self).sorted(byKeyPath: "Id")
     
-    func getResult() -> Results<License>{
-        return result
+    //全資格を返す
+    func getResult() -> List<License>{
+        return List<License>(result)
     }
     
+    //条件に合致した資格を返す
+    func getResult(_ query :String) -> List<License> {
+        let searchResult = try! Realm().objects(License.self).filter(query)
+        return List<License>(searchResult)
+    }
+    
+    //レコード追加
     func addLicense(_ licenseName :String) {
         let myLisence = License()
         
@@ -72,6 +82,7 @@ class RealmControllerLisence{
         }
     }
     
+    //レコードの全削除
     func allDelete(){
         try! realm.write {
             realm.delete(result)
@@ -79,36 +90,119 @@ class RealmControllerLisence{
     }
 }
 
-class RealmControllerQestion{
+class RealmControllerQuestion{
     let realm = try! Realm()
-    let result = try! Realm().objects(QesList.self).sorted(byKeyPath: "No")
+    let result :Results<Question>
+    
+    //RealmControllerQuestionのイニシャライザ　引数は資格ID
+    init(_ LicenseId :String) {
+        result = try! Realm().objects(Question.self).filter("LisenceId = \(LicenseId)").sorted(byKeyPath: "No")
+    }
 
-    func getResult() -> Results<QesList> {
-        return result
+    //全問題をリスト型で返す
+    func getResult() -> List<Question> {
+        return List<Question>(result)
     }
     
-    func getResult(_ query :String) -> Results<QesList> {
-        let queryResult = try! Realm().objects(QesList.self).filter(query)
-        return queryResult
+    //条件に合致した問題をリスト型で返す
+    func getResult(_ query :String) -> List<Question> {
+        let searchResult = result.filter(query)
+        return List<Question>(searchResult)
     }
+    
+    //未回答問題を指定された個数ランダムで返す
+    func getRandomResultNotDone(_ number :Int) -> List<Question> {
+        let searchResult = getRandomResult("Done = false", number)
+        return searchResult
+    }
+    
+    //未正解問題を指定された個数ランダムで返す
+    func getRandomResultNotCorrect(_ number :Int) -> List<Question>{
+        let searchResult = getRandomResult("Correct = false", number)
+        return searchResult
+    }
+ 
+    //全問題から指定した個数分ランダムソートで返す
+    func getRandomResult(_ number :Int) -> List<Question> {
+        let searchResult = result
+        let randomResult = List<Question>()
+        let count = searchResult.count - 1
+        var numbList :[Int] = []
+        
+        //0~resultCount-1までの数値の配列を作る
+        for i in 0..<(count){
+            numbList.append(i)
+        }
+        
+        //numblistをランダムに並べ替え
+        for i in 0..<(count) {
+            let ranI = Int(arc4random_uniform(UInt32(count - i)) + UInt32(i))
+            if i != ranI {
+                swap(&numbList[i] , &numbList[ranI])
+            }
+        }
+        
+        //randomResultにsearchResultを書き写す(countを超えた場合はそこまで返す)
+        for i in 0..<(number) {
+            if i > count {
+                break
+            }
+            randomResult.append(searchResult[numbList[i]])
+        }
+        
+        return randomResult
+    }
+    
+    //条件に合致した全問題を指定した個数分ランダムソートで返す
+    func getRandomResult(_ query :String , _ number :Int) -> List<Question> {
+        let searchResult = result.filter(query)
+        let randomResult = List<Question>()
+        let count = searchResult.count - 1
+        var numbList :[Int] = []
+        
+        //0~resultCount-1までの数値の配列を作る
+        for i in 0..<(count){
+            numbList.append(i)
+        }
+        
+        //numblistをランダムに並べ替え
+        for i in 0..<(count) {
+            let ranI = Int(arc4random_uniform(UInt32(count - i)) + UInt32(i))
+            if i != ranI {
+                swap(&numbList[i] , &numbList[ranI])
+            }
+        }
+        
+        //randomResultにsearchResultを書き写す(countを超えた場合はそこまで返す)
+        for i in 0..<(number) {
+            if i > count {
+                break
+            }
+            randomResult.append(searchResult[numbList[i]])
+        }
+        
+        return randomResult
+    }
+    
+    //※データベースのオブジェクト群を加工するメソッドはこちらで提供する
     
     func addQestion(_ newQesCsvSent :String) {
-        let myQes = QesList()
+        let myQes = Question()
         let ally = newQesCsvSent.components(separatedBy: ",")
         
         //ダウンキャストがうまくいかないとエラーが起こるので、場合分け必須
         myQes.No = ally[0] as! Int8
-        myQes.Genre = ally[1]
-        myQes.Problem = ally[2]
-        myQes.Comment = ally[3]
-        myQes.Option1 = ally[4]
-        myQes.Option2 = ally[5]
-        myQes.Option3 = ally[6]
-        myQes.Option4 = ally[7]
-        myQes.Answer = ally[8]
-        myQes.Correct = ally[9] as! Bool
-        myQes.Answered = ally[10] as! Bool
-        
+        myQes.LicenseId = ally[1] as! Int8
+        myQes.Genre = ally[2]
+        myQes.Problem = ally[3]
+        myQes.Comment = ally[4]
+        myQes.OptionA = ally[5]
+        myQes.OptionB = ally[6]
+        myQes.OptionC = ally[7]
+        myQes.OptionD = ally[8]
+        myQes.Answer = ally[9]
+        myQes.Correct = false
+        myQes.Done = false
         try! realm.write {
             realm.add(myQes)
         }
