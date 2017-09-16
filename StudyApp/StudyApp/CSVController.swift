5057ad1ff96c118cb8e7c3b9c44bcd7b0ff94493
Hirofumi <sasa1235711@gmail.com>
2017-09-16 12:26:39 +0900

    csvメソッド修正

--- StudyApp/StudyApp/CSVController.swift
+++ StudyApp/StudyApp/CSVController.swift
@@ -8,37 +8,36 @@
 
 import UIKit
 
-//問題クラス
+//CSVクラス
 class CSVController {
     
     let name = ""
     
-    let csvArr = ""
     
     func csvToArray () {
+        
         //csvのファイルパスを取得
         if let csvPath = Bundle.main.path(forResource: "question", ofType: "csv") {
             do {
+                
+                //RealmControllerQuestionのインスタンス生成
+                let realmControllerQuestion = RealmControllerQuestion(name)
+                
                 //csvのファイルのデータを所得
                 let csvStr = try String(contentsOfFile:csvPath, encoding:String.Encoding.utf8)
+                
                 //csvファイルを改行区切りで配列に格納
                 let csvArr = csvStr.components(separatedBy: .newlines)
-                print(csvArr)
+                //print(csvArr)
+                
+                for csvRow in csvArr {
+                    realmControllerQuestion.addQestion(csvRow)
+                }
+                
             } catch let error as NSError {
                 print(error.localizedDescription)
             }
         }
-    }
-    
-    //所定の条件のもとで問題を作成する
-    //各種フィールド値をセットしたら、このメソッドを実行する想定
-    //てかこれを動かさず他のメソッドを動かすとやばい
-    public func CSVimport() -> () {
-        
-        //RealmControllerQuestionのインスタンス生成
-        //とりあえずの実装なので改修予定 20170911
-        let realmControllerQuestion = RealmControllerQuestion(name)
-        realmControllerQuestion.addQestion(csvArr)
         
     }
 }
