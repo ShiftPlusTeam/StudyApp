b585461f1fa6b2315ecd33596aef37c746bdd5d1
Hirofumi <sasa1235711@gmail.com>
2017-10-16 23:21:50 +0900

    QuestionViewControllerクラスの追加

---
+++ StudyApp/StudyApp/QuestionViewController.swift
@@ -0,0 +1,65 @@
+//
+//  QuestionViewController.swift
+//  StudyApp
+//
+//  Created by Sasakura Hirofumi on 2017/10/16.
+//  Copyright © 2017年 ShiftPlus+. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    @IBOutlet weak var noLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var aSelect: UIButton!
+    @IBOutlet weak var bSelect: UIButton!
+    @IBOutlet weak var cSelect: UIButton!
+    @IBOutlet weak var dSelect: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    //Aボタンをタップ
+    @IBAction func selectAButton(_ sender: Any) {
+        
+    }
+    
+    //Bボタンをタップ
+    @IBAction func selectBButton(_ sender: Any) {
+        
+    }
+    
+    //Cボタンをタップ
+    @IBAction func selectCButton(_ sender: Any) {
+        
+    }
+    
+    //Dボタンをタップ
+    @IBAction func selectDButton(_ sender: Any) {
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
