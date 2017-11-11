ff195fed7bd7b247a45c9c6b6b55b4a7c5cd5f62
airshoh <shiftplus.airshoh@gmail.com>
2017-11-11 15:03:01 +0900

    前回分、プッシュ

--- StudyApp/StudyApp/ViewController.swift/CommentaryViewController.swift
+++ StudyApp/StudyApp/ViewController.swift/CommentaryViewController.swift
@@ -6,24 +6,45 @@
 //  Copyright © 2017年 ShiftPlus+. All rights reserved.
 //
 
+
 import UIKit
 import Realm
 
 class CommentaryViewController: UIViewController{
-
-    @IBOutlet weak var correct: UILabel!
-    @IBOutlet weak var option: UILabel!
-    @IBOutlet weak var comment: UILabel!
+    
+    
+    
+    @IBOutlet weak var correctlabel: UILabel!
+    @IBOutlet weak var optionlabel: UILabel!
+    @IBOutlet weak var commentlabel: UILabel!
+    
+    //画像表示させる用(未実装)
+    @IBOutlet weak var answerjudge: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        correct.text = "◯or×"
-        option.text = "正解："
-        comment.text = "解説文"
-        //""←の中にデータベースの中身のパスを入れる？
+        //現在の問題を定義
+        let results = AppDataController.getCurrentQuestionData()
         
+        //回答した選択肢を定義
+        let answer = AppDataController.selectAnswer
+        
+        //各種表示の更新
+        optionlabel.text = answer
+        commentlabel.text = results.comment
+        correctlabel.text = results.answer
+        
+        //正誤判定(ゆくゆくは画像へ)
+        if answer == correctlabel.text{
+            correctlabel.text="◯";
+        }else{
+            correctlabel.text="×";
+        }
         
     }
-
+    
 }
+
+
