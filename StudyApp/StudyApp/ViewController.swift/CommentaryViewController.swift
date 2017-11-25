6d057a2f0da3902735a52e1293988c0c49324a3e
Shoh <shiftplus.airshoh@gmail.com>
2017-11-25 18:56:12 +0900

    解説画面に画面遷移機能追加（問題数選択画面以降へ遷移できないため、テスト未実施）

--- StudyApp/StudyApp/ViewController.swift/CommentaryViewController.swift
+++ StudyApp/StudyApp/ViewController.swift/CommentaryViewController.swift
@@ -12,8 +12,6 @@ import Realm
 
 class CommentaryViewController: UIViewController{
     
-    
-    
     @IBOutlet weak var correctlabel: UILabel!
     @IBOutlet weak var optionlabel: UILabel!
     @IBOutlet weak var commentlabel: UILabel!
@@ -42,9 +40,26 @@ class CommentaryViewController: UIViewController{
         }else{
             correctlabel.text="×";
         }
-        
     }
     
-}
-
-
+    let answercounter = AppDataController.currentCount
+    let selectcount = AppDataController.count
+ 
+        
+    //選択した問題数に達した場合、結果画面へ。達していない場合は、次の問いへ
+    //「次へ」ボタンを押下後、問題数選択画面で選択した値と現在の回答数がイコールか判断
+    @IBAction func nextbutton(_ sender: UIButton) {
+    if selectcount == answercounter {
+        
+        //イコールの場合、結果画面へ遷移する。↓参照元
+        //<https://i-app-tec.com/ios/tap_gesture.html>
+        performSegue(withIdentifier: "goRecord",sender: nil)
+    } else {
+        
+        //イコールではない場合、前の画面へ戻る。↓参照元
+        //<http://appleharikyu.jp/iphone/?p=1074>
+        self.dismiss(animated: true, completion: nil)
+    
+            }
+        }
+    }
