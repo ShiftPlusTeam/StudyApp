7670bac06b23b974411e1a129dd367f620f6b31e
Shoh <shiftplus.airshoh@gmail.com>
2017-11-26 18:57:32 +0900

    画像表示機能を実装

--- StudyApp/StudyApp/ViewController.swift/CommentaryViewController.swift
+++ StudyApp/StudyApp/ViewController.swift/CommentaryViewController.swift
@@ -16,13 +16,18 @@ class CommentaryViewController: UIViewController{
     @IBOutlet weak var optionlabel: UILabel!
     @IBOutlet weak var commentlabel: UILabel!
     
-    //画像表示させる用(未実装)
-
-    
+    //「Assets.xcassets」に入っている画像を定義する。
     @IBOutlet weak var imageView: UIImageView!
+    
+    var maruimage: UIImage!
+    var batsuimage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // バンドルした画像ファイルを読み込み
+        maruimage = UIImage(named: "mark_maru")
+        batsuimage = UIImage(named: "mark_batsu")
         
         //現在の問題を定義
         let results = AppDataController.getCurrentQuestionData()
@@ -35,26 +40,34 @@ class CommentaryViewController: UIViewController{
         commentlabel.text = results.comment
         correctlabel.text = results.answer
         
-        //正誤判定(ゆくゆくは画像へ)
-        if answer == correctlabel.text{
-            correctlabel.text="◯";
+        //正誤判定
+        if answer == results.answer{
+            imageView.image = maruimage;
         }else{
-            correctlabel.text="×";
+            imageView.image = batsuimage;
         }
     }
     
-    let answercounter = AppDataController.currentCount
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    //解答数を定義する
+    let answercount = AppDataController.currentCount
+    //問題数選択画面で選択した値を定義する
     let selectcount = AppDataController.count
  
         
     //選択した問題数に達した場合、結果画面へ。達していない場合は、次の問いへ
     //「次へ」ボタンを押下後、問題数選択画面で選択した値と現在の回答数がイコールか判断
     @IBAction func nextbutton(_ sender: UIButton) {
-    if selectcount == answercounter {
+    if selectcount == answercount {
         
         //イコールの場合、結果画面へ遷移する。↓参照元
         //<https://i-app-tec.com/ios/tap_gesture.html>
         performSegue(withIdentifier: "goRecord",sender: nil)
+        
     } else {
         
         //イコールではない場合、前の画面へ戻る。↓参照元
